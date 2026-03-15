6af85637fca62fb134fdb9dd127dafa535430f2f
bravobryan <120871658+bravobryan@users.noreply.github.com>
2026-03-15 06:12:21 -0700

    finished fred_import.ipynb

--- StraitofHormuzResearch/code_notes.py
+++ StraitofHormuzResearch/code_notes.py
@@ -1,24 +1,27 @@
-fred_fx_series = {
-    "euro_area": "DEXUSEU",
-    "japan": "DEXJPUS",
-    "china": "DEXCHUS",
-    "canada": "DEXCAUS",
-    "united_kingdom": "DEXUSUK",
-    "south_korea": "DEXKOUS",
-    "mexico": "DEXMXUS",
-    "india": "DEXINUS",
-    "brazil": "DEXBZUS",
-    "australia": "DEXUSAL",
-    "switzerland": "DEXSZUS",
-    "sweden": "DEXSDUS",
-    "norway": "DEXNOUS",
-    "denmark": "DEXDNUS",
-    "hong_kong": "DEXHKUS",
-    "singapore": "DEXSIUS",
-    "south_africa": "DEXSFUS",
-    "taiwan": "DEXTAUS",
-    "thailand": "DEXTHUS",
-    "new_zealand": "DEXUSNZ",
-    "malaysia": "DEXMAUS",
-    "venezuela": "DEXVZUS"
-}
+import os
+import sys
+
+# Set JAVA_HOME before importing PySpark and use findspark
+os.environ['JAVA_HOME'] = r'C:\Program Files\Java\jdk-22'  # May need to remove or update in cloud environment.
+import findspark
+findspark.init()
+
+import requests
+import pandas as pd
+import numpy as np
+import json
+import pyspark
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+from functools import reduce
+from pyspark.sql import (
+    functions as F,
+    Window as W,
+    types as T,
+    SparkSession,
+    DataFrame
+)
+
+# api keys and other hardcoded values for the Strait of Hormuz Research project.
+# Note: In a production environment, these should be stored securely and not hardcoded.
+import hardcoded_keys # Note: This file is added to .gitignore to prevent accidental commits of sensitive information.
